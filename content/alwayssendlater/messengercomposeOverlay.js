35206965465c6838ae82025bb21e91125fffddfb
YUKI Hiroshi <shimoda@clear-code.com>
2013-09-20 14:36:25 +0900

    Replace functions with delay.
    Confirm-Address and some addons redefines SendMessage() and SendMessageWithCheck() when the script is just loaded. So we have to do replacings after other addons are completely initialized.

--- content/alwayssendlater/messengercomposeOverlay.js
+++ content/alwayssendlater/messengercomposeOverlay.js
@@ -2,10 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-window.SendMessage = function() {
-  SendMessageLater();
-};
+(function() {
+  window.addEventListener('DOMContentLoaded', function onDOMContentLoaded() {
+    window.removeEventListener('DOMContentLoaded', onDOMContentLoaded, false);
 
-window.SendMessageWithCheck = function() {
-  SendMessageLater();
-};
+    window.SendMessage = function() {
+      SendMessageLater();
+    };
+
+    window.SendMessageWithCheck = function() {
+      SendMessageLater();
+    };
+  }, false);
+})();
